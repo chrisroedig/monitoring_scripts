daccfdee8ede9fb6f432c6278268c7a6e7af3474
Christoph A. Roedig <croedig@covermymeds.com>
2019-08-05 16:56:55 -0400

    ability to save configs

--- config.py
+++ config.py
@@ -20,3 +20,16 @@ class Config():
     def child_config(self, d, name):
         val = self.__class__(d=d, source = self._source+"::"+name )
         return val
+    def path(self):
+        return self._source.split("::")
+    def save(self, keyname, value):
+        filename = self.path()[0]
+        file_dict = yaml.load(open(filename, 'r').read())
+        working_dict = file_dict
+        self._data[keyname] = value
+        for k in self.path()[1:]:
+            working_dict= working_dict[k]
+        working_dict.update(self._data)
+        stream = open(filename, 'w')
+        yaml.dump(file_dict, stream, default_flow_style=False)
+
