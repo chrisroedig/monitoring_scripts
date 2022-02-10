f999cabcf2dbd00b3731c5844c7cb59039c1a04b
Chris <chris@roedig.us>
2022-02-10 08:35:56 -0500

    get speedtest working

--- sources/speedtest.py
+++ sources/speedtest.py
@@ -1,47 +1,59 @@
-import data_client
-import config
 import speedtest
-import dateutil
+from sources.data_source import DataSourceBase
+from sources.data_source import DataPayload
+from datetime import datetime
 
-# TODO: refactor this ...
-# # Configuration and InfluxDB client
-# cfg = config.Config()
-# scfg = cfg.speedtest
-# dc = data_client.DataClient(database=scfg.influx_database)
+class DataSource(DataSourceBase):
+    def __init__(self, config):
+        self.config = config.speedtest
+        self.data = None
+        if self.config is None:
+            raise Exception('no configuration for speedtest found')
+    def get(self):
+        # Set up Speed Test
+        servers = []
+        threads = None
 
-# # Set up Speed Test
-# servers = []
-# threads = None
+        s = speedtest.Speedtest()
+        s.get_servers(servers)
+        s.get_best_server()
 
-# s = speedtest.Speedtest()
-# s.get_servers(servers)
-# s.get_best_server()
+        # Perform Speed Test and grab results
+        s.download(threads=threads)
+        s.upload(threads=threads)
+        self.data = s.results.dict()
 
-# # Perform Speed Test and grab results
-# s.download(threads=threads)
-# s.upload(threads=threads)
-# st_data = s.results.dict()
+    def payload(self):
+        # Tag the measurement
+        tags = {
+            'location': self.config.location_tag,
+            'isp' : self.data['client']['isp'],
+            'ip': self.data['client']['ip'],
+            'server_host' : self.data['server']['host']
+        }
 
-# # Tag the measurement
-# tags = {
-#     'location': '395Riley',
-#     'isp' : st_data['client']['isp'],
-#     'ip': st_data['client']['ip'],
-#     'server_host' : st_data['server']['host']
-# }
+        # Structure the measurement data
+        fields = {
+            'upload_rate_bits': self.data['upload'],
+            'download_rate_bits': self.data['download'],
+            'ping_latency_ms': self.data['ping'],
+            'bytes_sent': self.data['bytes_sent'],
+            'bytes_received': self.data['bytes_received']
+        }
+        return SpeedTestPayload(tags, fields)
+    
+    def payloads(self):
+        if self.data is None:
+            self.get()
+        return [
+            self.payload()
+        ]
 
-# # Structure the measurement data
-# data = {
-#     'upload_rate_bits': st_data['upload'],
-#     'download_rate_bits': st_data['download'],
-#     'ping_latency_ms': st_data['ping'],
-#     'bytes_sent': st_data['bytes_sent'],
-#     'bytes_received': st_data['bytes_received']
-# }
-
-# # parse the given timestamp
-# timestamp = dateutil.parser.isoparse(st_data['timestamp'])
-
-# # write the data to influxdb
-# dc.write('speedtest', fields=data, tags=tags, timestamp=timestamp)
-
+class SpeedTestPayload(DataPayload):
+    def __init__(self, tags, fields):
+        super().__init__(tags, fields)
+        self.topic = ['isp_speedtest']
+        self.tags = tags
+        self.fields = fields
+    def __repr__(self):
+        return f'<SpeedTestPayload {self.topic} up: {self.fields["upload_rate_bits"]} down: {self.fields["download_rate_bits"]}>'
